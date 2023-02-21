fcef6a136353aec092403de12179e6cb99c154e6
zoghbi <you@example.comhisham.zoghbi@adesso.de>
2023-02-21 14:05:45 +0100

    repo angepasst

--- src/main/java/de/zoghbi/mongodb/repository/StudentRepository.java
+++ src/main/java/de/zoghbi/mongodb/repository/StudentRepository.java
@@ -2,12 +2,14 @@ package de.zoghbi.mongodb.repository;
 
 import de.zoghbi.mongodb.model.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * @author hisham.zoghbi or 07.02.23
  */
+@Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
     Optional<Student> findStudentByEmail(String email);
 }
